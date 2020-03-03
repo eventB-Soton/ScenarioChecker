22a97be3dcdd65f5eb88525d2777d61bc5215227
cfsnook <cfs@soton.ac.uk>
2020-03-03 23:39:02 +0000

    update participant

--- ac.soton.eventb.scenariochecker/src/ac/soton/eventb/internal/scenariochecker/participants/ScenarioCheckerParticipant.java
+++ ac.soton.eventb.scenariochecker/src/ac/soton/eventb/internal/scenariochecker/participants/ScenarioCheckerParticipant.java
@@ -2,21 +2,27 @@ package ac.soton.eventb.internal.scenariochecker.participants;
 
 import org.eventb.core.IMachineRoot;
 
-import ac.soton.eventb.internal.scenariochecker.views.SimulatorView;
 import ac.soton.eventb.probsupport.IAnimationParticipant;
+import ac.soton.eventb.scenariochecker.SimulationManager;
 
 public class ScenarioCheckerParticipant implements IAnimationParticipant {
 	
+	/* (non-Javadoc)
+	 * @see ac.soton.eventb.probsupport.IAnimationParticipant#startAnimating(org.eventb.core.IMachineRoot)
+	 */
 	@Override
 	public void startAnimating(IMachineRoot mchRoot) {
-		
-		
-		SimulatorView.getSimulatorView().initialise(mchRoot);
+		if (SimulationManager.getDefault().isOpen()) {
+			SimulationManager.getDefault().initialise(mchRoot);
+		}
 	}
 	
+	/* (non-Javadoc)
+	 * @see ac.soton.eventb.probsupport.IAnimationParticipant#stopAnimating(org.eventb.core.IMachineRoot)
+	 */
 	@Override
 	public void stopAnimating(IMachineRoot mchRoot) {
-		//SimulationManager.getSimulatorView().dispose();
+		SimulationManager.getDefault().stop(mchRoot);
 	}
 
 }
