fba43a9e4accaf0e1aeb31cd4582111b8bbb314e
cfsnook <cfs@soton.ac.uk>
2020-05-04 15:44:49 +0100

    revised to focus on oracle file handling.. playback is now removed

--- ac.soton.eventb.scenariochecker/src/ac/soton/eventb/internal/scenariochecker/OracleHandler.java
+++ ac.soton.eventb.scenariochecker/src/ac/soton/eventb/internal/scenariochecker/OracleHandler.java
@@ -11,10 +11,8 @@
 package ac.soton.eventb.internal.scenariochecker;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.eclipse.core.commands.ExecutionException;
@@ -28,8 +26,6 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.emf.common.command.Command;
-import org.eclipse.emf.common.util.EList;
-import org.eclipse.emf.common.util.EMap;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
@@ -48,17 +44,12 @@ import org.eclipse.ui.PlatformUI;
 import org.eventb.emf.core.CorePackage;
 import org.eventb.emf.core.EventBNamed;
 import org.eventb.emf.core.EventBObject;
-import org.eventb.emf.core.machine.Event;
 import org.eventb.emf.core.machine.Machine;
 
-import ac.soton.eventb.emf.oracle.Entry;
 import ac.soton.eventb.emf.oracle.Oracle;
 import ac.soton.eventb.emf.oracle.OracleFactory;
 import ac.soton.eventb.emf.oracle.OraclePackage;
 import ac.soton.eventb.emf.oracle.Run;
-import ac.soton.eventb.emf.oracle.Snapshot;
-import ac.soton.eventb.emf.oracle.Step;
-import ac.soton.eventb.probsupport.data.Operation_;
 import ac.soton.eventb.scenariochecker.Activator;
 
 
@@ -72,6 +63,13 @@ public class OracleHandler {
 	
 	private final static String oracleExtension = "oracle";
 	private boolean debug = true;
+
+	private Shell shell = null;
+	private Machine machine = null;
+	private IFolder folder = null;
+	private TransactionalEditingDomain editingDomain;
+	private String oracleName = null;
+	private IFolder oracleFolder = null;
 	
 	/**
 	 * Singleton
@@ -94,251 +92,22 @@ public class OracleHandler {
 	 * SET UP
 	 */
 	
-	public void initialise(EventBObject eventBObject){
+	public void initialise(String name, EventBObject eventBObject){
 		shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
 		machine = (Machine) ((EventBNamed)eventBObject.getContaining(CorePackage.Literals.EVENT_BNAMED_COMMENTED_COMPONENT_ELEMENT));
 		try {
 			folder = getOracleFolder(eventBObject);
 		} catch (CoreException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			if (debug) System.out.println("Oracle initialisation FAILED");
 		}
-		playback = false;
+		oracleName = name;
+		for (Resource resource : editingDomain.getResourceSet().getResources()){
+			resource.unload();
+		}		
 	}
-	
-	public boolean restart(String name, EventBObject eventBObject){
-		if (eventBObject==null){
-			if (debug)System.out.println("Oracle initialisation FAILED due to no machine");
-			return false;
-		}else{
-			machine = (Machine) ((EventBNamed)eventBObject.getContaining(CorePackage.Literals.EVENT_BNAMED_COMMENTED_COMPONENT_ELEMENT));
-			try {
-				folder = getOracleFolder(eventBObject);
-			} catch (CoreException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				if (debug) System.out.println("Oracle initialisation FAILED");
-			}
-			oracleName = name;
-			for (Resource resource : editingDomain.getResourceSet().getResources()){
-				resource.unload();
-			}		
-			if (playback==true) doStartPlayback();
-//			else  doStartRecording();
-			return true;
-		}
-	}
-	
-	
-//	/**
-//	 * @param goldSnapshot
-//	 * @param newSnapshot
-//	 * @return
-//	 */
-//	private boolean compareSnapshots(Snapshot goldSnapshot, Snapshot newSnapshot) {
-//		EMap<String, String> newValues = newSnapshot.getValues();
-//		for (Map.Entry<String, String> goldValue : goldSnapshot.getValues()){
-//			if (!(newValues.containsKey(goldValue.getKey()) && newValues.get(goldValue.getKey()).equals(goldValue.getValue()))){
-//				return false;
-//			}
-//		}
-//		return true;
-//	}
 
-
-	/**
-	 * When in playback, returns the next snapshot.
-	 * 
-	 * If not in playback or a step is next, returns null;
-	 * 
-	 * @return
-	 */
-	//	 * This assumes that the stepPointer is pointing to the last executed step
-	public Snapshot getNextGoldSnapshot() {
-		if (!isPlayback() || currentPlaybackRun == null){return null;}
-		EList<Entry> oracleEntries = currentPlaybackRun.getEntries();
-		if (stepPointer <0 || stepPointer+1 >= oracleEntries.size() || !(oracleEntries.get(stepPointer+1) instanceof Snapshot))
-			{return null;}
-		return (Snapshot) oracleEntries.get(stepPointer+1);
-	}
-	
-	/**
-	 * PLAYBACK
-	 */
-	
-	/**
-	 * Checks whether currently in playback mode 
-	 * @return
-	 */
-	public boolean isPlayback(){
-		return playback;
-	}
-	
-	/**
-	 * Switch to playback mode
-	 * If repeat is true, use the existin oracle file, otherwise a new one will be loaded
-	 * 
-	 * @param repeat
-	 */
-	public void startPlayback(boolean repeat){
-		playback = true;
-		this.repeat = repeat;
-	}
-	
-	/**
-	 * 
-	 */
-	private void doStartPlayback(){
-		playback = true;
-		if (debug) System.out.println("Oracle startPlayback");
-		if (currentPlaybackRun == null || repeat==false){
-			currentPlaybackRun = getGoldRun();
-		}
-		//if acquiring gold run failed, revert to recording mode
-		if (currentPlaybackRun == null) playback = false;
 		
-		stepPointer = -1;
-		snapShotPointer = -1;
-		nextStep = null;
-//		doStartRecording();
-		return ;
-	}
-	
-	/**
-	 * This consumes the next step so that the next find will return a different operation.
-	 * 
-	 * @param animator
-	 * @return
-	 */
-	public void consumeNextStep() {
-		nextStep = null;
-		snapShotPointer = stepPointer+1;
-	}
-	
-	/** 
-	 * Returns the next operation according to the Oracle being replayed
-	 * this does not change the Oracle state so can be called repeatedly
-	 * 
-	 * @return operation
-	 */
-	public Operation_ findNextOperation() {
-		if (!hasNextStep() && isPlayback()){
-			stopPlayback();
-			return null;
-		}
-		
-		if (nextStep!=null){
-			List<String> args = new ArrayList<String>();
-			args.addAll(nextStep.getArgs());
-			if (debug) System.out.println("Oracle selected next operation = "+nextStep.getName());
-			return (new Operation_(nextStep.getName(),args));
-			
-//			for (Operation_ op : ops) {
-//				if (op.getName().equals(nextStep.getName())) {
-//					boolean thisOne;
-//					if (nextStep.getArgs().size() == op.getArguments().size()){
-//						thisOne = true;
-//						Iterator<String> it = nextStep.getArgs().iterator();
-//						for (String arg : op.getArguments()){
-//							if (!(it.hasNext() && arg.equals(it.next()))){
-//								thisOne = false;
-//								break;
-//							}
-//						}						
-//					}else{
-//						thisOne = false;
-//					}
-//					if (thisOne) {
-//						if (debug) System.out.println("Oracle selected next operation = "+op.getName());
-//						return op;
-//					}
-//				}
-//			}
-		}
-//		MessageBox mbox = new MessageBox(shell, SWT.ICON_ERROR | SWT.OK);
-//		mbox.setText("Playback Discrepancy");
-//		String message = "?";
-//		if (nextStep ==null){
-//			message = "Original operation is null";
-//		}else{
-//			message = "Original operation is not enabled in playback. \n Operation_ = "+nextStep.getName();
-//			if (nextStep.getArgs().size()>0){
-//				message = message+"\n"+"Arguments = "+nextStep.getArgs() ;
-//			}
-//		}
-//		mbox.setMessage(message);
-//		mbox.open();
-
-		if (debug) System.out.println("Oracle select next operation FAILED");
-		return null; 
-	}
-	
-	/**
-	 * @return
-	 */
-	private boolean hasNextStep(){
-		assert(playback==true);
-		if (nextStep == null) {
-			EList<Entry> oracleEntries = currentPlaybackRun.getEntries();
-			int oldStepPointer = stepPointer;
-			Event event;
-			do{
-				stepPointer = stepPointer+1;
-			}while (stepPointer < oracleEntries.size() && (!(oracleEntries.get(stepPointer) instanceof Step) ||
-					//TODO: do we need this.. i don't think we store internal events any more
-					(event = Utils.findEvent(((Step)oracleEntries.get(stepPointer)).getName(), machine))!=null &&
-					Utils.isInternal(event))) ;
-			if (stepPointer < oracleEntries.size()){
-				nextStep = (Step)oracleEntries.get(stepPointer);
-				return true;
-			} else {
-				stepPointer = oldStepPointer;
-				return false;
-			}
-		}
-		return true;
-	}
-	
-	/**
-	 * Stop playback from the current oracle file
-	 * @param save
-	 * @return
-	 */
-	public boolean stopPlayback() {
-		assert(playback==true);
-		if (debug) System.out.println("Oracle stopPlayback" ); //(save = "+save+")");
-		playback = false;
-		return true;
-	}
-	
-	////////////////////////////////
-	/////////////internal//////////
-	////////////////////////////////
-	private Shell shell = null;
-	private Machine machine = null;
-	private IFolder folder = null;
-	private TransactionalEditingDomain editingDomain;
-	private String oracleName = null;
-		
-	//PLAYBACK
-	//flag indicating that same gold should be played back again
-	private boolean repeat = false;
-	// flag indicating playback in progress
-	private boolean playback = false;
-	// the Run being played back - this should not be modified
-	private Run currentPlaybackRun = null;
-	// this is a cache of the next step to be taken. getNextStep returns this if it is not null.
-	// It should be cleared to null when it has successfully been used so that getnextStep is forced to retrieve another step.
-	private Step nextStep= null;	
-	// pointer into the collection of playback Entries - used to find the next step to be taken
-	private Integer stepPointer = -1; 
-	
-	// pointer into the collection of Entries - used to find the next Snapshot 
-	// (when a step is recorded, this is moved to the entry after that step).
-	private Integer snapShotPointer = -1;
-
-	private IFolder oracleFolder = null;
 
 	/**
 	 * @return
@@ -368,7 +137,7 @@ public class OracleHandler {
 	/**
 	 * @return
 	 */
-	private Run getGoldRun() {
+	public Run getRun() {
 		try {
 		   FileDialog dialog = new FileDialog(shell, SWT.OPEN);
 		   dialog.setFilterExtensions(new String [] {"*."+oracleExtension,"*.*"});
